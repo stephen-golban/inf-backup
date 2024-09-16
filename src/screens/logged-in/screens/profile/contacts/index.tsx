87bf90c41b587d2d234e72413e0e1c408fbdffc3
ioncojocari <i.cojocari@staffcapital.nl>
2024-09-16 20:58:24 +0300

    disable/enable push notifications from oneSignal

--- src/screens/logged-in/screens/profile/contacts/index.tsx
+++ src/screens/logged-in/screens/profile/contacts/index.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 import { useContacts } from './hooks';
 
+import { Loader } from '@components/ui';
 import { ContactsModule } from '@modules/logged-in';
-import { ActivityIndicator } from '@components/common';
 import { PROFILE_SCREENS, ProfileStackScreenProps } from '@typings/navigation';
 
 const ContactsScreen: React.FC<ProfileStackScreenProps<PROFILE_SCREENS.CONTACTS>> = () => {
   const { contacts, loading } = useContacts();
 
   if (loading) {
-    return <ActivityIndicator fill justify="center" />;
+    return <Loader fill justify="center" />;
   }
 
   return <ContactsModule contacts={contacts!} />;
--- src/typings/env.d.ts
+++ src/typings/env.d.ts
@@ -1,6 +1,7 @@
 declare module '@env' {
   export const API_URL: string;
   export const VERSION_CODE: string;
+  export const APP_VERSION: string;
   export const APP_DISPLAY_NAME: string;
   export const BUNDLE_IDENTIFIER: string;
   export const PRIVATE_KEY_STORAGE: string;
