1073c803b5633dc11ff9290278fb40bc20a166d6
ioncojocari <i.cojocari@staffcapital.nl>
2024-11-04 11:22:07 +0200

    MAIN-549-fixes: firebase events

--- src/services/me/index.ts
+++ src/services/me/index.ts
@@ -4,12 +4,19 @@ import { useTryCatch } from '@library/hooks';
 import { setAppUser, useAppStore } from '@store/app';
 
 import type { User } from '@typings/user';
+import { useEffect } from 'react';
+import { logEvent, setUser } from '../../../firebaseEvents';
 
 function useMe(runOnMount = true) {
   const me = useAppStore(state => state.user);
   const [call, { loading }] = useLazyAxios<User>({ method: 'get', url: '/admin-api/person' });
 
   const getMe = useTryCatch(async () => await call(undefined, res => setAppUser(res)));
+
+  useEffect(() => {
+    logEvent('app_start');
+    setUser(String(me?.id));
+  }, [me]);
 
   if (runOnMount) {
     useMount(getMe);
